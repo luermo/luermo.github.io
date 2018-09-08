4d3fab393c7efd81ac5816de7eab1d5eee540143
Eric Garcia <eric@webyo.es>
2018-09-08 12:52:24 +0200

    update info

--- assets/modules/formation.php
+++ assets/modules/formation.php
@@ -39,13 +39,13 @@
             <div class="panel panel-primary">
                 <div class="panel-heading">
                     <h4 class="panel-title">
-                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationOne">
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationTwo">
                             <span>Postgrado IT Governance</span>
                             <strong class="pull-right hidden-xs">UPC School  (2013)</strong>
                         </a>
                     </h4>
                 </div>
-                <div id="collapseEducationOne" class="panel-collapse collapse in">
+                <div id="collapseEducationTwo" class="panel-collapse collapse in">
                     <div class="panel-body">
                         <img class="pull-right" src="../assets/img/upc.gif" alt="UPC">
                         <ul>
@@ -63,13 +63,13 @@
             <div class="panel panel-primary">
                 <div class="panel-heading">
                     <h4 class="panel-title">
-                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationTwo">
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationThree">
                             <span>Grado en Ingeniería Informática, Ingeniería de Software</span>
                             <strong class="pull-right hidden-xs">Universitat Politècnica de Catalunya   (2011 -2013)</strong>
                         </a>
                     </h4>
                 </div>
-                <div id="collapseEducationTwo" class="panel-collapse collapse">
+                <div id="collapseEducationThree" class="panel-collapse collapse">
                     <div class="panel-body">
                         <img class="pull-right hidden-xs" src="../assets/img/upc.gif" alt="UPC">
                         <p>Capacita para participar en proyectos de desarrollo, mantenimiento y evaluación de servicios y sistemas software de naturaleza diversa. La participación puede ser ocupando varios roles, desde director de proyecto, analista, experto de dominio, ingeniero de requisitos, arquitecto del software, administrador de bases de datos, y un largo etcétera. Los proyectos pueden ser de complejidad arbitraria, de criticidad máxima (con responsabilidades que pueden incluso hacer depender vidas humanas de su correcto funcionamiento) y de natura técnica diversa. Como disparo diferencial, capacita para asegurar que estos proyectos cumplirán criterios de calidad reconocidos y actuales en la disciplina, serán desarrollados con rigor máximo y usando conocimiento muy diverso, y tendrán en cuenta aspectos sociales como por ejemplo la sostenibilidad, la gestión óptima de recursos y la privacidad.</p>
@@ -80,13 +80,13 @@
             <div class="panel panel-primary">
                 <div class="panel-heading">
                     <h4 class="panel-title">
-                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationThree">
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationFor">
                             <span>Técnico Superior en Desarrollo de Aplicaciones Informáticas</span>
                             <strong class="pull-right hidden-xs">Joan Brossa  (2009 -2011)</strong>
                         </a>
                     </h4>
                 </div>
-                <div id="collapseEducationThree" class="panel-collapse collapse">
+                <div id="collapseEducationFor" class="panel-collapse collapse">
                     <div class="panel-body">
                         <p>Capacita para desarrollar aplicaciones informáticas, participando en el diseño y realizando la programación de pruebas y documentación de las mismas de conformidad con los requisitos funcionales, especificaciones aprobadas y de acuerdo con la normativa vigente. Su trabajo se ubica fundamentalmente en las funciones/subfunciones de investigación y desarrollo (I+D), en análisis, diseño, producción y verificación de productos "software", en empresas de servicios informáticos, en empresas grandes y medianas dentro del departamento de proceso de datos y en la Administración Pública (Central, Autonómica, Local).</p>
                     </div>
@@ -95,13 +95,13 @@
             <div class="panel panel-primary">
                 <div class="panel-heading">
                     <h4 class="panel-title">
-                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationFor">
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationFive">
                             <span>Técnico Superior en Gestión Comercial y Márqueting</span>
                             <strong class="pull-right hidden-xs">Agustí Serra and Fontanet (2006 -2008)</strong>
                         </a>
                     </h4>
                 </div>
-                <div id="collapseEducationFor" class="panel-collapse collapse">
+                <div id="collapseEducationFive" class="panel-collapse collapse">
                     <div class="panel-body">
                         <p>Capacita para obtener, procesar y organizar la información referida al mercado: productos, servicios, precios, distribución y comunicación; gestionar los planes de actuación correspondientes a las compras, logística y venta de productos y/o servicios. Podrá asimismo planificar y dirigir las actuaciones de "merchandising" en el establecimiento comercial y realizar trabajos de campo, obteniendo, procesando y organizando la información en los ámbitos de la investigación comercial y de la acción publicitaria.</p>				
                     </div>
@@ -110,13 +110,13 @@
             <div class="panel panel-primary">
                 <div class="panel-heading">
                     <h4 class="panel-title">
-                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationFive">
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationSix">
                             <span>Técnico en Instalaciones Eléctricas y Automáticas</span>
                             <strong class="pull-right hidden-xs">Col•legi Montserrat (2002 -2004)</strong>
                         </a>
                     </h4>
                 </div>
-                <div id="collapseEducationFive" class="panel-collapse collapse">
+                <div id="collapseEducationSix" class="panel-collapse collapse">
                     <div class="panel-body">
                         <p>Montar y mantener infraestructuras de telecomunicación en edificios, instalaciones eléctricas de baja tensión, máquinas eléctricas y sistemas automatizados, aplicando normativa y reglamentación vigente, protocolos de calidad, seguridad y riesgos laborales, asegurando su funcionamiento y respeto al medio ambiente. Ejerce su actividad en pequeñas y medianas empresas, mayoritariamente privadas, dedicadas al montaje y mantenimiento de infraestructuras de comunicación en edificios, máquinas eléctricas, sistemas automatizados, instalaciones eléctricas de baja tensión y sistemas domóticos, bien por cuenta propia o por cuenta ajena.</p>				
                     </div>
