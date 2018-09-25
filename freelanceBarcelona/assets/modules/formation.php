1d3b01a36b3e46ba4f520dca778c96371818b4ab
Didac Chaves <didac@gooapps.net>
2018-09-25 10:59:53 +0200

    add assets

---
+++ freelanceBarcelona/assets/modules/formation.php
@@ -0,0 +1,127 @@
+<section class="section resume" id="formacion">
+    <div class="container">
+        <h2 class="heading" style="color: #428bca;">FORMACIÓN</h2>
+        <div class="panel-group" id="accordionEducation">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationOne">
+                            <span>MBA & Máster en Dirección Comercial y Marketing</span>
+                            <strong class="pull-right hidden-xs">ENEB - Escuela de Negocios Europea de Barcelona  (2017)</strong>
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseEducationOne" class="panel-collapse collapse ">
+                    <div class="panel-body">
+                        <img class="pull-right" src="../assets/img/eneb.png" alt="ENEB">
+                        <ul>
+                            <li>Establecer, gestionar y controlar los elementos financieros clave de la empresa.</li>
+<li>Tener una visión global y, de este modo, promover el pensamiento crítico.</li>
+<li>Conocer todos los departamentos del negocio y desenvolverte hábilmente en ellos.</li>
+<li>Conocer los modelos que favorecen la excelencia en la gestión y las implicaciones de las nuevas formas organizativas así como desarrollar habilidades de organización que ayuden a poner en práctica de manera eficiente nuevas políticas dentro del contexto de la organización.</li>
+<li>Desarrollar habilidades de liderazgo en el trabajo en equipo.</li>
+<li>Valorar e implementar una adecuada gestión de responsabilidad social corporativa.</li>
+<li>Gestionar eficientemente las TIC e implementarlas en el desarrollo de nuestras habilidades.</li>
+<li>Conocer las herramientas estratégicas de comunicación, control, calidad y cambio organizacional.</li>
+<li>Dirección de las campañas comerciales y de marketing.</li>
+<li>Análisis de la competencia y aportar valor añadido a nuestra marca.</li>
+<li>Investigación de mercados y su posterior análisis.</li>
+<li>Creación, gestión y dirección de la comunicación dentro de las campañas de marketing.</li>
+<li>Dirección de iniciativas comerciales y supervisión de las mismas.</li>
+<li>Análisis de los diferentes mercados nacionales e internacionales y adaptar las campañas de marketing a cada territorio.</li>
+                        </ul>
+
+
+                        <div style="text-align:center;" ><a class="btn btn-default" href="https://eneb.es/masters-y-posgrados/mba-_-master-en-direccion-comercial-y-marketing/" target="_blank">Más información</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationTwo">
+                            <span>Postgrado IT Governance</span>
+                            <strong class="pull-right hidden-xs">UPC School  (2013)</strong>
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseEducationTwo" class="panel-collapse collapse in">
+                    <div class="panel-body">
+                        <img class="pull-right" src="../assets/img/upc.gif" alt="UPC">
+                        <ul>
+                            <li>Aprender de manera integral los conceptos, métodos y tecnologías relevantes en la gestión de proyectos informáticos.</li>
+                            <li>Gestionar de forma eficiente una cartera de proyectos en una organización, con las consecuentes problemáticas, diferentes de la propia gestión individual de un único proyecto.</li>
+                            <li>Estudiar el conjunto de actividades que comporta la gobernanza IT, un factor clave en las organizaciones que se enfrentan a la gestión de un numeroso grupo de proyectos.</li>
+                            <li>Exponer los aspectos de gestión IT que son específicos de una organización con orientación a servicios.</li>
+                        </ul>
+
+
+                        <div style="text-align:center;" ><a class="btn btn-default" href="http://www.talent.upc.edu/professionals/presentacio/idioma/ing/codi/300500/" target="_blank">Más información</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationThree">
+                            <span>Grado en Ingeniería Informática, Ingeniería de Software</span>
+                            <strong class="pull-right hidden-xs">Universitat Politècnica de Catalunya   (2011 -2013)</strong>
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseEducationThree" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <img class="pull-right hidden-xs" src="../assets/img/upc.gif" alt="UPC">
+                        <p>Capacita para participar en proyectos de desarrollo, mantenimiento y evaluación de servicios y sistemas software de naturaleza diversa. La participación puede ser ocupando varios roles, desde director de proyecto, analista, experto de dominio, ingeniero de requisitos, arquitecto del software, administrador de bases de datos, y un largo etcétera. Los proyectos pueden ser de complejidad arbitraria, de criticidad máxima (con responsabilidades que pueden incluso hacer depender vidas humanas de su correcto funcionamiento) y de natura técnica diversa. Como disparo diferencial, capacita para asegurar que estos proyectos cumplirán criterios de calidad reconocidos y actuales en la disciplina, serán desarrollados con rigor máximo y usando conocimiento muy diverso, y tendrán en cuenta aspectos sociales como por ejemplo la sostenibilidad, la gestión óptima de recursos y la privacidad.</p>
+                        <div style="text-align:center;" ><a class="btn btn-default" href="http://www.fib.upc.edu/en/estudiar-enginyeria-informatica/especialitats-grau/enginyeria-del-software.html" target="_blank">Més informació</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationFor">
+                            <span>Técnico Superior en Desarrollo de Aplicaciones Informáticas</span>
+                            <strong class="pull-right hidden-xs">Joan Brossa  (2009 -2011)</strong>
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseEducationFor" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <p>Capacita para desarrollar aplicaciones informáticas, participando en el diseño y realizando la programación de pruebas y documentación de las mismas de conformidad con los requisitos funcionales, especificaciones aprobadas y de acuerdo con la normativa vigente. Su trabajo se ubica fundamentalmente en las funciones/subfunciones de investigación y desarrollo (I+D), en análisis, diseño, producción y verificación de productos "software", en empresas de servicios informáticos, en empresas grandes y medianas dentro del departamento de proceso de datos y en la Administración Pública (Central, Autonómica, Local).</p>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationFive">
+                            <span>Técnico Superior en Gestión Comercial y Márqueting</span>
+                            <strong class="pull-right hidden-xs">Agustí Serra and Fontanet (2006 -2008)</strong>
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseEducationFive" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <p>Capacita para obtener, procesar y organizar la información referida al mercado: productos, servicios, precios, distribución y comunicación; gestionar los planes de actuación correspondientes a las compras, logística y venta de productos y/o servicios. Podrá asimismo planificar y dirigir las actuaciones de "merchandising" en el establecimiento comercial y realizar trabajos de campo, obteniendo, procesando y organizando la información en los ámbitos de la investigación comercial y de la acción publicitaria.</p>				
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordionEducation" href="#collapseEducationSix">
+                            <span>Técnico en Instalaciones Eléctricas y Automáticas</span>
+                            <strong class="pull-right hidden-xs">Col•legi Montserrat (2002 -2004)</strong>
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseEducationSix" class="panel-collapse collapse">
+                    <div class="panel-body">
+                        <p>Montar y mantener infraestructuras de telecomunicación en edificios, instalaciones eléctricas de baja tensión, máquinas eléctricas y sistemas automatizados, aplicando normativa y reglamentación vigente, protocolos de calidad, seguridad y riesgos laborales, asegurando su funcionamiento y respeto al medio ambiente. Ejerce su actividad en pequeñas y medianas empresas, mayoritariamente privadas, dedicadas al montaje y mantenimiento de infraestructuras de comunicación en edificios, máquinas eléctricas, sistemas automatizados, instalaciones eléctricas de baja tensión y sistemas domóticos, bien por cuenta propia o por cuenta ajena.</p>				
+                    </div>
+                </div>
+            </div>
+
+        </div>
+</section>
---
+++ freelanceBarcelona/assets/modules/projects.php
@@ -0,0 +1,183 @@
+
+<section class="section projects" id="proyectos">
+    <div class="container">
+        <h2 class="heading">PROYECTOS</h2>
+        <p class="text-center">Esta es una pequeña muestra de los últimos proyectos que he desarrollado</p>
+        <div class="row project-list">
+            <ul class="project-grid project-effect-shrink">
+                <li class="col-md-6">
+                    <figure>
+                        <img src="../assets/img/projects/gooapps.jpg" alt="Goo Apps"/>
+                        <figcaption>
+                            <div class="project-item-info">
+                                <h3 class="sub-heading">Goo Apps</h3>
+                                <p></p>
+                                <a href="http://gooapps.net/" target="_blank" class="btn btn-default">Visitar proyecto</a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="col-md-6">
+                    <figure>
+                        <img src="../assets/img/projects/gooMedical.png" alt="Goo Medical"/>
+                        <figcaption>
+                            <div class="project-item-info">
+                                <h3 class="sub-heading">Goo Medical</h3>
+                                <p></p>
+                                <a href="http://goomedical.es/" target="_blank" class="btn btn-default">Visitar proyecto</a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="col-md-6">
+                    
+                    <figure>
+                        <img src="../assets/img/projects/goocheckDevices.png" alt="Goo Check"/>
+                        <figcaption>
+                            <div class="project-item-info">
+                                <h3 class="sub-heading">Goo Check</h3>
+                                <p></p>
+                                <a href="http://goocheck.es/" target="_blank" class="btn btn-default">Visitar proyecto</a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="col-md-6">
+                    <figure>
+                        <img src="../assets/img/projects/timecare.png" alt="Timecare"/>
+                        <figcaption>
+                            <div class="project-item-info">
+                                <h3 class="sub-heading">TimeCare</h3>
+                                <p></p>
+                                <a href="http://timecare.es/" target="_blank" class="btn btn-default">Visitar proyecto</a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="col-md-6">
+                    <figure>
+                        <img src="../assets/img/projects/goocampus.png" alt="gooCampus"/>
+                        <figcaption>
+                            <div class="project-item-info">
+                                <h3 class="sub-heading">gooCampus</h3>
+                                <p></p>
+                                <a href="http://goocampus.com/" target="_blank" class="btn btn-default">Visitar proyecto</a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="col-md-6">
+                    <figure>
+                        <img src="../assets/img/projects/e-testament.png" alt="e-testament"/>
+                        <figcaption>
+                            <div class="project-item-info">
+                                <h3 class="sub-heading">e-testament</h3>
+                                <p></p>
+                                <a href="http://etestament.cat/" target="_blank" class="btn btn-default">Visitar proyecto</a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="col-md-6">
+                    <figure>
+                        <img src="../assets/img/projects/Sernino.png" alt="sernino"/>
+                        <figcaption>
+                            <div class="project-item-info">
+                                <h3 class="sub-heading">SerNiño</h3>
+                                <p>Plataforma de contratación de professionales para niños.</p>
+                                <a href="http://www.serniño.com/" target="_blank" class="btn btn-default">Visitar proyecto</a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="col-md-6">
+                    <figure>
+                        <img src="../assets/img/projects/smartConf.jpg" alt="smartconf"/>
+                        <figcaption>
+                            <div class="project-item-info">
+                                <h3 class="sub-heading">SmartConf.</h3>
+                                <p>Aplicación para congresos, eventos y confererncias</p>
+                                <a href="http://getsmartConf.com/" target="_blank" class="btn btn-default">Visitar proyecto</a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="col-md-6">
+                    <figure>
+                        <img src="../assets/img/projects/BBM.jpg" alt="bbm"/>
+                        <figcaption>
+                            <div class="project-item-info">
+                                <h3 class="sub-heading">BBM barcelona breast meeting</h3>
+                                <p>Aplicación multiplataforma para un congreso especifico sobre medicina</p>
+                                <a href="http://bbm.cat/" target="_blank" class="btn btn-default">Visitar proyecto</a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+
+                <li class="col-md-6">
+                    <figure>
+                        <img src="../assets/img/projects/visitum.png" alt="visitum"/>
+                        <figcaption>
+                            <div class="project-item-info">
+                                <h3 class="sub-heading">Get My Guide</h3>
+                                <p>Aplicación web para la distribución y venta de tours online</p>
+                                <a href="http://www.get-my-guide.com" target="_blank" class="btn btn-default">Visitar proyecto</a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="col-md-6">
+                    <figure>
+                        <img src="../assets/img/projects/trackone.jpg" alt="trackone"/>
+                        <figcaption>
+                            <div class="project-item-info">
+                                <h3 class="sub-heading">Track One Service</h3>
+                                <p>Sistema de gestión de trabajos para empresas</p>
+                                <a href="http://trackoneservice.com/" target="_blank" class="btn btn-default">Visitar proyecto</a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                
+                <li class="col-md-6">
+                    <figure>
+                        <img src="../assets/img/projects/yourcoded.png" alt="yourcoded"/>
+                        <figcaption>
+                            <div class="project-item-info">
+                                <h3 class="sub-heading">yourcoded.com</h3>
+                                <p>Servicio de generación de códigos QR para productos de consumo</p>
+                                <a href="http://www.yourcoded.com" target="_blank" class="btn btn-default">Visitar proyecto</a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="col-md-6">
+                    <figure>
+                        <img src="../assets/img/projects/bit4services.jpg" alt="bit4services"/>
+                        <figcaption>
+                            <div class="project-item-info">
+                                <h3 class="sub-heading">bit4services</h3>
+                                <p>Entorno de demostración de las soluciones de Bit4d</p>
+                                <a href="http://bit4services.com/" target="_blank" class="btn btn-default">Visitar proyecto</a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="col-md-6">
+                    <figure>
+                        <img src="../assets/img/projects/indra.png" alt="indra"/>
+                        <figcaption>
+                            <div class="project-item-info">
+                                <h3 class="sub-heading">Indra bmb - Gestión documental</h3>
+                                <p>Sistema de gestión documental</p>
+                                <a href="http://www.indracompany.com/en/soluciones-y-servicios/solucion/document-management/1620/summary" target="_blank" class="btn btn-default">Visitar proyecto</a>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </li>
+                
+            </ul>
+        </div>
+    </div>	
+</section>
